d83f68796dbb2959f9e4691f717983c63147b4e7
Iago <iagojlourenco@gmail.com>
2021-12-09 02:14:59 -0300

    heroku

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ app.use(routes);
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server started on port", PORT);
 });

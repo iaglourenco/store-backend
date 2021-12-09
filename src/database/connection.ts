138876d00248fe7a421fde1533405ec2e5377c57
Iago <iagojlourenco@gmail.com>
2021-12-09 11:28:40 -0300

    h

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,3 +1,14 @@
 import { createConnection } from "typeorm";
 
-createConnection();
+createConnection({
+  type: "sqlite",
+  database: "./src/database/database.sqlite",
+  migrations: [
+    "./src/database/migrations/*.ts",
+    "./src/database/migrations/*.js",
+  ],
+  entities: ["./src/models/*.ts", "./dist/src/models/*.js"],
+  cli: {
+    migrationsDir: "./src/database/migrations",
+  },
+});
